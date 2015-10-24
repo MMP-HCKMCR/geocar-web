f00be65648872487b537385afc06a0164ae66100
Simon Harris <simon@pearfalse.com>
2015-10-24 13:48:59 +0100

    Basic WCF service and unit test

---
+++ GeoCar.WcfService.Test/WebApiServiceTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GeoCar.WcfService.Test
+{
+    [TestClass]
+    public class WebApiServiceTests
+    {
+        [TestMethod]
+        public void TestMarco()
+        {
+            var target = new WebApiService();
+
+            Assert.AreEqual("polo", target.marco());
+        }
+    }
+}
---
+++ GeoCar.WcfService/WebApiService.svc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace GeoCar.WcfService
+{
+    public class WebApiService : IWebApiService
+    {
+        [WebGet]
+        public string marco()
+        {
+            return "polo";
+        }
+    }
+}
