d09ced5ae16e7384f013b128a3e227035d4219a3
unknown <MMP-HCKMCR>
2015-10-24 22:06:11 +0100

    Addition of Transaction Data Access

---
+++ GeoCar.Database/TransactionRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeoCar.Model;
+
+namespace GeoCar.Database
+{
+    public class TransactionRepository
+    {
+        public static Transaction CreateTransaction(int userId, int points, int transactionTypeId)
+        {
+            var parameters = new List<SqlParameter>
+            {
+                new SqlParameter
+                {
+                    ParameterName = "userId",
+                    Value = userId
+                },
+                new SqlParameter
+                {
+                    ParameterName = "points",
+                    Value = points
+                },
+                new SqlParameter
+                {
+                    ParameterName = "transactionTypeId",
+                    Value = transactionTypeId
+                }
+            };
+
+            var dataTable = DatabaseCommon.PerformAction("AddTransaction", parameters);
+
+            return dataTable != null && dataTable.Rows.Count > 0 ? PopulateTransaction(dataTable.Rows[0]) : null;
+        }
+
+        private static Transaction PopulateTransaction(DataRow transaction)
+        {
+            return new Transaction
+            {
+                TransactionId = transaction.Field<int>("TransactionId"),
+                UserId = transaction.Field<int>("UserId"),
+                Points = transaction.Field<int>("Points"),
+                TimeCaptured = transaction.Field<DateTime>("TimeCaptured"),
+                TransactionTypeId = transaction.Field<int>("TransactionTypeId")
+            };
+        }
+    }
+}
---
+++ GeoCar.Model/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoCar.Model
+{
+    public class Transaction
+    {
+        public int TransactionId { get; set; }
+        public int UserId { get; set; }
+        public int Points { get; set; }
+        public DateTime TimeCaptured { get; set; }
+        public int TransactionTypeId { set; get; }
+    }
+}
