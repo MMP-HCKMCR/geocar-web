6acb74edfe1e079d4060670d78d0d07484f824fd
Simon Harris <simon@pearfalse.com>
2015-10-24 17:00:26 +0100

    Add Login method; Marco now POST

---
+++ GeoCar.WcfService/Requests/SessionRequestObjects.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.Serialization;
+
+namespace GeoCar.WcfService.Requests
+{
+    public class LoginRequest
+    {
+        [DataMember]
+        public string EmailAddress { get; set; }
+
+        [DataMember]
+        public string UserPassword { get; set; }
+    }
+}
