3920834b4f5337b81b6bde24a490f7e1c7154b29
unknown <MMP-HCKMCR>
2015-10-24 19:08:40 +0100

    Addition of Initial Tag Details

---
+++ GeoCar.Database/TagRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using GeoCar.Model;
+
+namespace GeoCar.Database
+{
+    internal static class TagRepository
+    {
+        public static Tag RetrieveTag(string UUID, int majorNumber, int minorNumber)
+        {
+            var parameters = new List<SqlParameter>();
+
+            parameters.Add(new SqlParameter
+            {
+                ParameterName = "UUID",
+                Value = UUID
+            });
+
+            parameters.Add(new SqlParameter
+            {
+                ParameterName = "majorNumber",
+                Value = majorNumber
+            });
+
+            parameters.Add(new SqlParameter
+            {
+                ParameterName = "minorNumber",
+                Value = minorNumber
+            });
+            
+            var dataTable = DatabaseCommon.PerformAction("GetTagForUUIDMajorAndMinorNumber", parameters);
+
+            return dataTable != null && dataTable.Rows.Count > 0 ? PopulateTag(dataTable.Rows[0]) : null;
+        }
+
+        private static Tag PopulateTag(DataRow tag)
+        {
+            return new Tag
+            {
+                TagId = tag.Field<int>("TagId"),
+                UUID = tag.Field<Guid>("UUID").ToString(),
+                MajorNumber = tag.Field<int>("MajorNumber"),
+                MinorNumber = tag.Field<int>("MinorNumber"),
+                Active = tag.Field<bool>("Active"),
+                AdditionalPoints = tag.Field<int>("AdditionalPoints"),
+                TagTypeId = tag.Field<int>("TagTypeId")
+            };
+        }
+    }
+}
---
+++ GeoCar.Model/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoCar.Model
+{
+    public class Tag
+    {
+        public int TagId { get; set; }
+        public string UUID { get; set; }
+        public int MajorNumber { get; set; }
+        public int MinorNumber { get; set; }
+        public bool Active { get; set; }
+        public int AdditionalPoints { get; set; }
+        public int TagTypeId { get; set; }
+    }
+}
