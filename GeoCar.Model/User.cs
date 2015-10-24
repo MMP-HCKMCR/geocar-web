54657fc89691af145acea69fb76b8edd0165b329
unknown <MMP-HCKMCR>
2015-10-24 15:39:16 +0100

    Start of Data Layer

---
+++ GeoCar.Model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeoCar.Model
+{
+    public class User
+    {
+        public int UserId { set; get; }
+        public string Email { set; get; }
+
+    }
+}
