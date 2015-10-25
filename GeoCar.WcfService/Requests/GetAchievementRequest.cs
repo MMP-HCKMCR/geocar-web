70ad9b7e288927d78c27caae8acc03f36192a8d2
unknown <MMP-HCKMCR>
2015-10-25 02:30:32 +0000

    Addition of Get Achievement API

---
+++ GeoCar.WcfService/Requests/GetAchievementRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Runtime.Serialization;
+using GeoCar.WcfService.Responses;
+
+namespace GeoCar.WcfService.Requests
+{
+    public class GetAchievementRequest : ApiResult
+    {
+        [DataMember]
+        public Guid SessionId { get; set; }
+    }
+}
---
+++ GeoCar.WcfService/Responses/AchievementResponse.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.Serialization;
+
+namespace GeoCar.WcfService.Responses
+{
+    public class AchievementResponse
+    {
+        [DataMember]
+        public string AchievementName { set; get; }
+        [DataMember]
+        public string AchievementDescription { set; get; }
+    }
+}
